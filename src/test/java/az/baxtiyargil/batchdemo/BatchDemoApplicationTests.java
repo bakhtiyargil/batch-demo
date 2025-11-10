325aff3bf35b22b354025382928d01fb0492d523
Bakhtiyar V. Karimov <bext97@mail.ru>
2025-11-11 00:36:43 +0400

    Initial commit.

---
+++ src/test/java/az/baxtiyargil/batchdemo/BatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package az.baxtiyargil.batchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
