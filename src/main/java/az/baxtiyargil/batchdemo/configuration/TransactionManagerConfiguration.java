2b4ca9bed101a9dc600fe35f1d3cea88fb077676
Bakhtiyar V. Karimov <bext97@mail.ru>
2025-11-11 18:31:45 +0400

    Add transaction manager and batch job configurations

---
+++ src/main/java/az/baxtiyargil/batchdemo/configuration/TransactionManagerConfiguration.java
@@ -0,0 +1,27 @@
+package az.baxtiyargil.batchdemo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class TransactionManagerConfiguration {
+
+    /**
+     * Spring automatically detects this bean and uses it as the default transaction manager.
+     * It coordinates transaction boundaries (commit/rollback) for JDBC operations performed
+     * using the given {@code DataSource}.
+     *
+     * @param dataSource the DataSource to manage transactions for.
+     * @return a new {@link org.springframework.jdbc.datasource.DataSourceTransactionManager}
+     * used by the Batch Job Repository and all transaction-related operations in the project.
+     */
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
